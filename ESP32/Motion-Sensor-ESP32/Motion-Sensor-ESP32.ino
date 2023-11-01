ab1bf00c9bb7e57da2ae133359eb59d3e6c0368e
AimanIrfan01 <81033855+AimanIrfan01@users.noreply.github.com>
2023-11-01 18:27:31 +0800

    Improve security
    - Added email and password for authentication motion sensor.

--- ESP32/Motion-Sensor-ESP32/Motion-Sensor-ESP32.ino
+++ ESP32/Motion-Sensor-ESP32/Motion-Sensor-ESP32.ino
@@ -2,6 +2,7 @@
 #include <HTTPClient.h>
 #include <Firebase_ESP_Client.h>
 #include <Wire.h>
+#include "time.h"
 
 //Provide the token generation process info.
 #include "addons/TokenHelper.h"
@@ -9,32 +10,53 @@
 #include "addons/RTDBHelper.h"
 
 // Insert your network credentials
-#define WIFI_SSID "arfa33_2.4G"
-#define WIFI_PASSWORD "blackjack66"
+//#define WIFI_SSID "arfa33_2.4G"
+//#define WIFI_PASSWORD "blackjack66"
+
+//#define WIFI_SSID "XZ1"
+//#define WIFI_PASSWORD "test1234"
+
+#define WIFI_SSID "Hotspot@PUTRA-2.4G"
+#define WIFI_PASSWORD ""
 
 // Insert Firebase project API Key
 #define API_KEY "AIzaSyDlVO_Ne9zTNjZljeu0_cFscLWzzJ1ppJo"
+//#define API_KEY "AIzaSyBvcyHfUINuFXYdeXl8WHz4499yqZZPRPo"
 
 // Insert RTDB URL
 #define DATABASE_URL "https://fyp-esp32-19f8b-default-rtdb.asia-southeast1.firebasedatabase.app/"
+//#define DATABASE_URL "https://putra-scada-default-rtdb.asia-southeast1.firebasedatabase.app/"
 
 // Messenger Firebase
 const char* serverKey = "AAAAzF5r-7M:APA91bFcGa5IMhtAH2D0nUq2PW-KjDk1zoDJ8S1i563FWqYsLcucx5UzFkS8Mz0VP97j4aCQdegJN0MvaeV8wKonLEWU_3yNTmqgOhJVkWcbxP6EnfLxgEU_AzRgIMxsbQoIOrrYZdxG";
+//const char* serverKey = "AAAA1MTGS4M:APA91bELhwLBWmOeE4Td484ZVV7WjT4SVzEYLbh_UteI3ecuw02Oe5yx-CpRy-8kAfxZFme7s2lcEgYwpoJY-KMuDgMyOsB77814ZyXtrnxbusBnuMyndN9fksvp3FGgokHsci1H0uf4";
 
+#define USER_EMAIL "motionsensor@gmail.com"
+#define USER_PASSWORD "motionsensor1234"
 
 // Define Firebase Data object
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
 bool signupOK = false;
-String token;
+String token = "";
 int motionActive = 0;
+String uid;
+String wifiIP;
 
-String wifiIP = "";
+// Variable to save current epoch time
+unsigned long epochTime;
+
+// NTP server to request epoch time
+const char* ntpServer = "pool.ntp.org";
+const long  gmtOffset_sec = 0;
+const int   daylightOffset_sec = 8 * 3600;
+String Time;
+String Date;
 
 const int motionSensor = 4;
-int CurrentStat = LOW;
-int PrevState = LOW;
+int currentStat = LOW;
+int prevStat = LOW;
 
 void setup() {
   Serial.begin(115200);
@@ -42,30 +64,43 @@ void setup() {
   connectToWiFi();
   configFB();
   getToken();
-  pinMode(motionSensor, INPUT_PULLUP);
+  pinMode(motionSensor, INPUT);
+  Serial.println("Exit setup()");
 }
 
 void loop() {
-  // Store previous state 
-  PrevState = CurrentStat;
-  CurrentStat = digitalRead(motionSensor);
+  // Store previous state
+  prevStat = currentStat;
+  currentStat = digitalRead(motionSensor);
 
   getMotionActive();
 
   // If detect motion and in mobile phone it is in on mode
-  if (PrevState == LOW && CurrentStat == HIGH && motionActive == 1) {
-    Serial.println("Motion detected!\nGoing to push notification");
-    sendFCMNotification("Hello from ESP32!", "This is a test message from ESP32.");
-    motionActive = 0;
+  //  if (PrevState == LOW && CurrentStat == HIGH && motionActive == 1) {
+  //    Serial.println("Motion detected!\nGoing to push notification");
+  //    sendFCMNotification("Hello from ESP32!", "This is a test message from ESP32.");
+  //    motionActive = 0;
+  //  }
+  //  // Detect motion and in mobile phone it is in off mode
+  //  else if (PrevState == LOW && CurrentStat == HIGH){
+  //    Serial.println("Motion detected!\nMotion Active is in disable mode");
+  //  }
+  if (prevStat == LOW && currentStat == HIGH) {
+    if (motionActive == 1) {
+      sendFCMNotification("Alert!", "There is unusual movemoment on Kitchen window!");
+      Serial.println("Message alert send!");
+      delay(3000);
+    }
+    else {
+      Serial.println("Message alert not send due to motion active is disable");
+    }
+
   }
-  // Detect motion and in mobile phone it is in off mode
-  else if (PrevState == LOW && CurrentStat == HIGH){
-    Serial.println("Motion detected!\nMotion Active is in disable mode");
-  }
+
   delay(1000);
 }
 
-void sendFCMNotification(const char* title, const char* body) {
+void sendFCMNotification(String title, String body) {
   HTTPClient http;
   http.begin("https://fcm.googleapis.com/fcm/send");
   http.addHeader("Content-Type", "application/json");
@@ -116,22 +151,37 @@ void configFB() {
   /* Assign the api key (required) */
   config.api_key = API_KEY;
 
+  // Assign the user sign in credentials
+  auth.user.email = USER_EMAIL;
+  auth.user.password = USER_PASSWORD;
+
   /* Assign the RTDB URL (required) */
   config.database_url = DATABASE_URL;
 
   /* Sign up */
-  if (Firebase.signUp(&config, &auth, "", "")) {
-    Serial.println("FB Connection okay");
-    signupOK = true;
-  } else {
-    Serial.printf("%s\n", config.signer.signupError.message.c_str());
-  }
+  //  if (Firebase.signUp(&config, &auth, "", "")) {
+  //    Serial.println("FB Connection okay");
+  //    signupOK = true;
+  //  } else {
+  //    Serial.printf("%s\n", config.signer.signupError.message.c_str());
+  //  }
 
   /* Assign the callback function for the long-running token generation task */
   config.token_status_callback = tokenStatusCallback;  // see addons/TokenHelper.h
 
   Firebase.begin(&config, &auth);
   Firebase.reconnectWiFi(true);
+
+  // Getting the user UID might take a few seconds
+  Serial.println("Getting User UID");
+  while ((auth.token.uid) == "") {
+    Serial.print('.');
+    delay(1000);
+  }
+  // Print user UID
+  uid = auth.token.uid.c_str();
+  Serial.print("User UID: ");
+  Serial.println(uid);
 }
 
 // get token value for smartphone
@@ -159,5 +209,36 @@ void getMotionActive() {
     }
   } else {
     Serial.println(fbdo.errorReason());
+    Serial.print("Motion Active:");
+    Serial.println(motionActive);
   }
+}
+
+void getLocationMotion() {
+  if (Firebase.RTDB.getString(&fbdo, "/SmartPhone/Token")) {
+    if (fbdo.dataType() == "string") {
+      token = fbdo.stringData();
+      Serial.print("Success\ntoken: ");
+      Serial.println(token);
+    }
+  } else {
+    Serial.println(fbdo.errorReason());
+  }
+
+}
+
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+
+  Time = String(timeinfo.tm_hour) + "-" + String(timeinfo.tm_min) + "-" + String(timeinfo.tm_sec);
+  Serial.print("Time: ");
+  Serial.println(Time);
+
+  Date = String(timeinfo.tm_year + 1900) + "-" + String(timeinfo.tm_mon + 1) + "-" + String(timeinfo.tm_mday);
+  Serial.print("Date: ");
+  Serial.println(Date);
 }
